8aeb28f3af768e2ac141e8bfb13602746794fb9e
Guy Haviv <theguyhaviv@gmail.com>
2024-09-20 10:06:16 +0300

    initial svelte commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <h1>Player Minutes Tracker</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+</main>
